922eb960f9d6a1fea339e978111e327ed064e2bb
Héctor Pérez Ambrosio <92091720+4mbr0s10@users.noreply.github.com>
2022-03-13 21:01:40 +0100

    Update Jugador.java

--- RunTimeException/src/runtimeexception/Jugador.java
+++ RunTimeException/src/runtimeexception/Jugador.java
@@ -15,26 +15,35 @@ import javafx.scene.image.ImageView;
 public class Jugador extends Personaje {
    private String urlImagen="file:spaceShipSingleShoot.png";
    private String urlImagenAtaque="File:Missile.png";
+   private int x;
+   private int y;
+   private int lX;
+   private int lY;
+   
     public Jugador(int hp) {
         super(hp);
     }
 
-    public Jugador(int hp, String urlImagen, String urlImagenAtaque) {
+    public Jugador(int hp, String urlImagen, String urlImagenAtaque, int x, int y, int lX, int lY) {
         super(hp);
         this.urlImagen = urlImagen;
         this.urlImagenAtaque=urlImagenAtaque;
+        this.x=x;
+        this.y=y;
+        this.lX=lX;
+        this.lY=lY;
     }
 
     public ImageView spriteIntegrada (Image image) {
         ImageView mv = new ImageView(image);
 
         mv.setRotate(-90);
-        mv.setLayoutX(-100);
-        mv.setLayoutY(200);
+        mv.setLayoutX(20);
+        mv.setLayoutY(330);
         mv.setFitHeight(100);
         mv.setFitWidth(100);
         mv.setPreserveRatio(true);
-
+     
 
         return mv;
     }
@@ -43,12 +52,12 @@ public class Jugador extends Personaje {
         ImageView mv3 = new ImageView(image3);
             
             mv3.setRotate(90);
-            mv3.setLayoutX(50);
+            mv3.setLayoutX(20);
             mv3.setLayoutY(325);
             mv3.setFitHeight(50);
             mv3.setFitWidth(50);
             mv3.setPreserveRatio(true);
-            
+           
             return mv3;
     }
 
@@ -66,6 +75,38 @@ public class Jugador extends Personaje {
 
     public void setUrlImagenAtaque(String urlImagenAtaque) {
         this.urlImagenAtaque = urlImagenAtaque;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getlX() {
+        return lX;
+    }
+
+    public void setlX(int lX) {
+        this.lX = lX;
+    }
+
+    public int getlY() {
+        return lY;
+    }
+
+    public void setlY(int lY) {
+        this.lY = lY;
     }
 
 }
